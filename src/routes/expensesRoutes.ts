0948fb699c52952aaec333615814b4c7ed2f7aa1
vishva-kalhara <vishvakalhara@gmail.com>
2024-05-03 15:35:46 +0530

    Schema Validation

--- src/routes/expensesRoutes.ts
+++ src/routes/expensesRoutes.ts
@@ -1,5 +1,34 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { z } from "zod";
+
+type Expense = {
+    _id: number;
+    title: string;
+    amount: number;
+};
+
+const fakeExpenses: Expense[] = [
+    { _id: 1, title: "Groceries", amount: 50 },
+    { _id: 2, title: "Gasoline", amount: 30 },
+    { _id: 3, title: "Dinner", amount: 60 },
+    { _id: 4, title: "Movie Tickets", amount: 25 },
+    { _id: 5, title: "Internet Bill", amount: 80 },
+];
+
+const postExpenseSchema = z.object({
+    title: z
+        .string()
+        .min(3, "Title should include more than 3 characters")
+        .max(25),
+    amount: z.number().int().positive(),
+});
 
 export const expensesRoutes = new Hono()
-    .get("/", (c) => c.json({ status: "success", data: "data" }))
-    .post("/", (c) => c.json({ data: "created" }));
+    .get("/", (c) => c.json(fakeExpenses))
+    .post("/", zValidator("json", postExpenseSchema), async (c) => {
+        const data = await c.req.valid("json");
+        const expense = { ...data, _id: fakeExpenses.length + 1 };
+        fakeExpenses.push(expense);
+        return c.json(expense);
+    });
